787957abd8adef31ba49ce55321425970aaff7fb
SebiB24 <sebycool242@gmail.com>
2025-04-25 23:43:43 +0300

    added form for adding wishes in the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,31 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { wishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   items: wishItem[] = [
-    new wishItem('To learn Angular'),
-    new wishItem('Get coffee', true),
-    new wishItem('To find grass that cuts itself', false)
+    // new wishItem('To learn Angular'),
+    // new wishItem('Get coffee', true),
+    // new wishItem('To find grass that cuts itself', false)
   ];
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  addNewWish(){
+    this.items.push(new wishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   toggleItem(item: wishItem){
     item.isComplete = !item.isComplete;
